91d1f2cb7e7ef6ea11cbd03800421a36d1c780f7
Klaus Hauschild <klaus.hauschild@xom-materials.com>
2020-10-21 08:45:39 +0200

    rewrite of prettify unit tests

--- test/impex/prettify.js
+++ test/impex/prettify.js
@@ -3,11 +3,11 @@ const assert = require('assert');
 
 const prettify = require('../../lib/impex/prettify');
 
-describe('impex', () => {
-    it('prettify', () => {
+describe('prettify', () => {
+    it('impex', () => {
         const malformed = fs.readFileSync('test/impex/malformed.impex').toString();
         const prettified = prettify(malformed);
         const pretty = fs.readFileSync('test/impex/pretty.impex').toString();
-        assert.equal(prettified, pretty);
+        assert.strictEqual(prettified, pretty);
     });
 });
