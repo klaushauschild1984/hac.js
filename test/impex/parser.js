6d9cc92e94a1b8986e1a192595d66849d89f5de3
Klaus Hauschild <klaus.hauschild@xom-materials.com>
2020-10-21 09:47:44 +0200

    couple of fixes

--- test/impex/parser.js
+++ test/impex/parser.js
@@ -38,6 +38,38 @@ describe('parser', () => {
             }
         });
     });
+    describe('data', () => {
+        it('empty cells', () => {
+            const tokens = parser(';super very long value that is longer than its header   ;;; ;;;');
+            assert.strictEqual(tokens.length, 1);
+            assert.strictEqual(tokens[0].type, 'data');
+            assert.strictEqual(tokens[0].value.length, 1);
+        });
+    });
+    describe('header', () => {
+        it('simple', () => {
+            const tokens = parser('INSERT_UPDATE Product; code');
+            assert.strictEqual(tokens.length, 1);
+            assert.strictEqual(tokens[0].type, 'header');
+            assert.strictEqual(tokens[0].value.mode, 'INSERT_UPDATE');
+            assert.strictEqual(tokens[0].value.columns.length, 1);
+            assert.strictEqual(tokens[0].value.columns[0].column, 'code');
+            assert.strictEqual(tokens[0].value.columns[0].modifiers.length, 0);
+        });
+        it('modifiers', () => {
+            const tokens = parser('iNsErT_uPdAtE Product; code [unique  =true ]  [allownull=true    ,default=code]; description[lang=en];approvalStatus(code)');
+            assert.strictEqual(tokens.length, 1);
+            assert.strictEqual(tokens[0].type, 'header');
+            assert.strictEqual(tokens[0].value.mode, 'INSERT_UPDATE');
+            assert.strictEqual(tokens[0].value.columns.length, 3);
+            assert.strictEqual(tokens[0].value.columns[0].column, 'code');
+            assert.strictEqual(tokens[0].value.columns[0].modifiers.length, 3);
+            assert.strictEqual(tokens[0].value.columns[1].column, 'description');
+            assert.strictEqual(tokens[0].value.columns[1].modifiers.length, 1);
+            assert.strictEqual(tokens[0].value.columns[2].column, 'approvalStatus(code)');
+            assert.strictEqual(tokens[0].value.columns[2].modifiers.length, 0);
+        });
+    });
     it('sample impex', () => {
         const impex = fs.readFileSync('test/impex/pretty.impex').toString();
         const tokens = parser(impex);
